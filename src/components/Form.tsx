9fb21b33d037b393e1a2db7e169c5bbfbc376998
Renan Marques <nan.dev01@gmail.com>
2023-12-22 14:24:26 -0300

    Create a style for the form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,6 +1,8 @@
 import Swal from 'sweetalert2';
 import { useState } from 'react';
 import { PasswordManagerType } from '../types/PasswordManagerType';
+import eyeClosedIcon from '../images/eye-closed.svg';
+import eyeOpenIcon from '../images/eye-open.svg';
 
 type FormProps = {
   onClickCancel: () => void,
@@ -53,51 +55,78 @@ function Form({ onClickCancel, onClickRegister }: FormProps) {
   };
 
   return (
-    <section>
-      <form>
-        <label htmlFor="service-name">Nome do serviço</label>
-        <input
-          id="service-name"
-          type="text"
-          value={ serviceName }
-          onChange={ ({ target }) => setServiceName(target.value) }
-        />
-        <label htmlFor="login">Login</label>
-        <input
-          id="login"
-          type="text"
-          value={ login }
-          onChange={ ({ target }) => setLogin(target.value) }
-        />
-        <label htmlFor="password">Senha</label>
-        <input
-          id="password"
-          type={ showPassword ? 'text' : 'password' }
-          value={ password }
-          onChange={ ({ target }) => setPassword(target.value) }
-        />
-        <button
-          data-testid="show-hide-form-password"
-          onClick={ (e) => handleClickShowPassword(e) }
-        >
-          mostrar/esconder senha
-        </button>
-        <label htmlFor="url">URL</label>
-        <input
-          id="url"
-          type="text"
-          value={ url }
-          onChange={ ({ target }) => setUrl(target.value) }
-        />
-        <button
-          disabled={ isDisabled }
-          onClick={ (e) => handleClickRegister(e) }
-        >
-          Cadastrar
-        </button>
-        <button onClick={ onClickCancel }>Cancelar</button>
+    <section className="form-container">
+      <form className="form">
+        <div className="service-container">
+          <label htmlFor="service-name">Nome do serviço</label>
+          <input
+            id="service-name"
+            type="text"
+            value={ serviceName }
+            onChange={ ({ target }) => setServiceName(target.value) }
+          />
+        </div>
+        <div className="login-password-container">
+          <div className="login-container">
+            <label htmlFor="login">Login</label>
+            <input
+              id="login"
+              type="text"
+              value={ login }
+              onChange={ ({ target }) => setLogin(target.value) }
+            />
+          </div>
+          <div className="password-container">
+            <label htmlFor="password">Senha</label>
+            <input
+              id="password"
+              type={ showPassword ? 'text' : 'password' }
+              value={ password }
+              onChange={ ({ target }) => setPassword(target.value) }
+            />
+            <button
+              className="eye-icon"
+              data-testid="show-hide-form-password"
+              onClick={ (e) => handleClickShowPassword(e) }
+            >
+              { showPassword
+                ? <img
+                    src={ eyeOpenIcon }
+                    alt="icone olho aberto"
+                />
+                : <img
+                    src={ eyeClosedIcon }
+                    alt="icone olho fechado"
+                />}
+            </button>
+          </div>
+        </div>
+        <div className="url-container">
+          <label htmlFor="url">URL</label>
+          <input
+            id="url"
+            type="text"
+            value={ url }
+            onChange={ ({ target }) => setUrl(target.value) }
+          />
+        </div>
+        <div className="form-btns-container">
+          <button
+            onClick={ onClickCancel }
+            className="form-cancel-btn"
+          >
+            Cancelar
+          </button>
+          <button
+            disabled={ isDisabled }
+            onClick={ (e) => handleClickRegister(e) }
+            className="form-register-btn"
+          >
+            Cadastrar
+          </button>
+        </div>
       </form>
-      <section>
+      <section className="validation-password-container">
         <p
           className={
             password.length >= 8 ? 'valid-password-check' : 'invalid-password-check'
@@ -132,6 +161,7 @@ function Form({ onClickCancel, onClickRegister }: FormProps) {
         >
           Possuir algum caractere especial
         </p>
+        <div className="triangle" />
       </section>
     </section>
   );
